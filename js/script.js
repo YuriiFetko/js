7f976b507dfb6eea06433a1a1c6c61224ecf226d
Yurii <fetkoyura@gmail.com>
2020-03-15 00:30:28 +0200

    add calc

--- js/script.js
+++ js/script.js
@@ -223,12 +223,54 @@ window.addEventListener("DOMContentLoaded", function () {
     plusSlides(1);
   });
 
-  dotsWrap.addEventListener('click', function(event) {
+  dotsWrap.addEventListener('click', function (event) {
     for (let i = 0; i < dots.length + 1; i++) {
       if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
         currentSlide(i);
       }
     }
   });
+
+  //Calc
+
+  let persons = document.querySelectorAll('.counter-block-input')[0],
+    restDays = document.querySelectorAll('.counter-block-input')[1],
+    place = document.getElementById('select'),
+    totalValue = document.getElementById('total'),
+    personsSum = 0,
+    daysSum = 0,
+    total = 0;
+
+  totalValue.innerHTML = '0';
+
+  persons.addEventListener('input', function () {
+    personsSum = +this.value;
+    total = (daysSum + personsSum) * 4000;
+    if (restDays.value == '' || personsSum == 0) {
+      totalValue.innerHTML = 0;
+    } else {
+      totalValue.innerHTML = total * place.options[place.selectedIndex].value;;
+    }
+  })
+
+  restDays.addEventListener('input', function () {
+    daysSum = +this.value;
+    total = (daysSum + personsSum) * 4000;
+
+    if (persons.value == '' || daysSum == 0) {
+      totalValue.innerHTML = 0;
+    } else {
+      totalValue.innerHTML = total * place.options[place.selectedIndex].value;;
+    }
+  })
+
+  place.addEventListener('input', function () {
+    if (restDays.value == '' || persons.value == '') {
+      totalValue.innerHTML = 0;
+    } else {
+      let a = total;
+      totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+    }
+  })
 
 });
